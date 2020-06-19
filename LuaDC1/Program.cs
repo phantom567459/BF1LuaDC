afc82bdd9e11aa556c9630a8a04fcf4d4f252e73
Dark_Phantom <patriotfan567@gmail.com>
2020-06-19 11:49:05 -0400

    code improvements

--- LuaDC1/Program.cs
+++ LuaDC1/Program.cs
@@ -30,10 +30,14 @@ namespace LuaDC1
             int globalcounter = 0;
             int globalcalledlast = 0;
 
+            string line;
             string[] localvariablelist = { };
+            string[] parser = { };
 
-            foreach (string line in lines)
+            //foreach (string line in lines)
+            for (int i = 0; i < lines.GetLength(0); i++) //used i to be able to hop around and iterate through lines a little better
             {
+                line = lines[i];
                 foreach (string x in vals)
                 {
                     if (line.Contains(x))
@@ -41,12 +45,12 @@ namespace LuaDC1
                         int index = line.IndexOf(x);
                         string newline = line.Substring(index);
                         Console.WriteLine(newline);
-                        string[] parser = newline.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                        parser = newline.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
                        
                         if (parser.GetLength(0) > 4)
                         {
-                            for (int i = 4; i < parser.GetLength(0); i++){
-                                parser[3] = String.Concat(parser[3], " ", parser[i]);
+                            for (int j = 4; j < parser.GetLength(0); j++){
+                                parser[3] = String.Concat(parser[3], " ", parser[j]);
                             }
                         }
                         
@@ -55,7 +59,7 @@ namespace LuaDC1
                             case "CREATETABLE":
                                 luafile = String.Concat(luafile,System.Environment.NewLine,"local table", tblCounter, " = { ");
                                 Console.WriteLine(String.Concat("local table",tblCounter," = { "));
-                                localvariablelist[localcounter] = String.Concat("table", tblCounter);
+                                //localvariablelist[localcounter] = String.Concat("table", tblCounter);
                                 int tblVals = int.Parse(parser[1]) * 2; //table and value associated with it
                                 tblDECL = 1;
                                 tblVarStatic = 1;
@@ -150,6 +154,30 @@ namespace LuaDC1
                                 break;
                             case "PUSHNIL":
                                 Console.WriteLine("nil");
+                                int numnil = int.Parse(parser[1]);
+                                for (int k = 0; k <= numnil; k++)
+                                {
+                                    if (k == 0 & numnil > 0)
+                                    {
+                                        luafile = String.Concat(luafile,"local local",localcounter,",");
+                                        localcounter += 1;
+                                    }
+                                    else if (k == 0 & numnil == 0)
+                                    {
+                                        luafile = String.Concat(luafile, "local local", localcounter, " = nil");
+                                        localcounter += 1;
+                                    }
+                                    else if (k < numnil & k != 0)
+                                    {
+                                        luafile = String.Concat(luafile, "local", localcounter, ",");
+                                        localcounter += 1;
+                                    }
+                                    else 
+                                    {
+                                        luafile = String.Concat(luafile, "local", localcounter, " = nil");
+                                        localcounter += 1;
+                                    }
+                                }
                                 break;
                             case "END":
                                 tblDECL = 0;
@@ -160,7 +188,6 @@ namespace LuaDC1
 
                         }
                         linecounter = linecounter + 1;
-
                     }
                 }
             }
